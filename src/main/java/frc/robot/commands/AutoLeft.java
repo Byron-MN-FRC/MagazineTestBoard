6b6308d89b030356e1ccd6c6b54d8aa6c3499fa4
NathanM-FRC <nmatte78@students.byron.k12.mn.us>
2021-09-13 19:36:38 -0500

    added autoShoot, minor bug fixes

--- src/main/java/frc/robot/commands/AutoLeft.java
+++ src/main/java/frc/robot/commands/AutoLeft.java
@@ -45,14 +45,14 @@ public class AutoLeft extends SequentialCommandGroup {
         //      )    
         //  );
         new zeroHood(m_ballShooter),
-        new setShootModeOn(m_ballShooter),
         new ParallelCommandGroup(
+            new setShootModeOn(m_ballShooter),
             new driveFeet(6.5, 0, m_driveTrain).withTimeout(3.5)
         ),
         
         new autoTurn(25, m_driveTrain),
         new turn2LimeLight(m_driveTrain),
-        new teleopAutoShootCMD(m_ballShooter),
+        new autonimousShoot(m_ballShooter),
         new autoTurn(-25, m_driveTrain),
         new extendSolenoid(m_ballAcquisition).withTimeout(3),
         new ParallelRaceGroup(
